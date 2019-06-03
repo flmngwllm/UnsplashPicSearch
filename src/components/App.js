257ab0001a1ac55c4663e686315cff42ce519d27
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-06-03 19:59:46 -0400

    addedasync await to the function and axios call to show the data

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,17 @@ import SearchBar from './SearchBar'
 
 
 class App extends React.Component{
-    onSearchSubmit(term){
-axios.get('https://api.unsplash.com/search/photos', {
-params: {query: term},
-headers: {
-    Authorization: 
-    'Client-ID d9126a95fc8e9c68c34d79693f8dab439d91bb08f38acc53a8067f14829605e3 '
+async onSearchSubmit(term){
+    const response = await axios
+        .get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 
+                'Client-ID d9126a95fc8e9c68c34d79693f8dab439d91bb08f38acc53a8067f14829605e3 '
+        }
+    })
+    console.log(response.data.results)
 }
-})
-    };
     render() {
     return (
         <div className="ui container" style={{ marginTop: '10px'}}>
